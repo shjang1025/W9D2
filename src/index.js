3242c5299d428772206436056319ba402b3e2ef8
shjang1025 <shjang1025@gmail.com>
2024-03-05 15:34:46 -0800

    make grid

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-Add your import statements for View and Game here
+import Game from "../ttt_node/game.js"
+import View from "./ttt-view.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const containerElement = document.querySelector('.ttt');
+  const game = new Game();
+  const view = new View(game, containerElement);
+
 });
--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,8 +1,26 @@
+
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    el.appendChild(this.setupBoard());
   }
   
   setupBoard() {
+      const ul = document.createElement("ul");
+      ul.style.width = "300px";
+
+      for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) {
+          const li = document.createElement("li");
+          li.setAttribute("data-pos", `[${i},${j}]`);
+         
+          ul.appendChild(li);
+
+        }
+      }
+      return ul;
+
   }
   
   handleClick(e) {
